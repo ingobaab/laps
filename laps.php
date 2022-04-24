3d4a27d5835084e830c7ccb63a767fb9d9364a22
Ingo Baab <ingo@baab.de>
2022-04-24 17:11:39 +0200

    Update laps.php
    Added correct plugin header for WordPress.org.

--- laps.php
+++ laps.php
@@ -1,16 +1,33 @@
 <?php
-
-/*
-Plugin Name: WordPress Profiler
-Plugin URI: https://github.com/ingobaab/laps
-Description: WordPress Profiler.
-Author: Ingo Baab
-Version: 3.3.5
-Author URI: https://www.rarst.net/
-Requires PHP: 7.1.3
-Update URI: false
-License: GPLv2
-*/
+/**
+ *
+ *  Profile Panel plugin for WordPress
+ *
+ * @package   profile-panel
+ * @link      https://github.com/ingobaab/laps
+ * @author    Ingo Baab <ingo@baab.de>
+ * @license   GPL v2 or later
+ *
+ * Plugin Name: Profile Panel
+ * Plugin URI: https://github.com/ingobaab/laps
+ * Description: A lightweight profile panel for WordPress.
+ * Author: Ingo Baab
+ * Version: 3.3.5
+ * Author URI: https://www.baab.de/
+ * Requires PHP: 7.1.3
+ * Update URI: false
+ * License: GPLv2
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+**/
 
 if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
 	require __DIR__ . '/vendor/autoload.php';
